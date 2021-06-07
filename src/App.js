7782f0c82a8064540eb6bf80ffedac23614c1a0a
Sam-McCathie <sammccathie2@gmail.com>
2021-06-07 15:33:21 +1200

    Css changes

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import {Route} from "react-router-dom";
 import Home from "./Components/Pages/Home/Home";
 // import GetQuote from "./Components/Pages/GetQuote/GetQuote";
 // import PersonalDetails from "./Components/Pages/PersonalDetails/PersonalDetails";
-import ReviewBuy from "./Components/Pages/ReviewBuy/ReviewBuy";
+// import ReviewBuy from "./Components/Pages/ReviewBuy/ReviewBuy";
+// import Thankyou from "./Components/Pages/ThankYou/Thankyou";
+import StateChange from "./Components/Pages/StateChange/StateChange";
 
 //add exact before path
 function App() {
   return (
     <div className="App">
       <Route exact path="/" component={Home} />
-      <Route exact path="/testing" component={ReviewBuy} />
+      <Route exact path="/testing" component={StateChange} />
     </div>
   );
 }
