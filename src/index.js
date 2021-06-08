fd9a4b9ea524ed2873813852800a5faefbe97b7c
Sam-McCathie <sammccathie2@gmail.com>
2021-06-08 14:13:03 +1200

    add root path to the basename prop of BrowserRouter

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {BrowserRouter} from "react-router-dom";
 import "@fontsource/roboto";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>,
   document.getElementById("root")
